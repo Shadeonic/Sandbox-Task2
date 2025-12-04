380b9818c8013bd5342f95f535c4bd24e3ca498e
Denisenko <aominebas2006@gmail.com>
2025-12-04 15:42:03 +0200

    Initialize FAQ feature and UI component folders

---
+++ src/features/faq/index.ts
@@ -0,0 +1,9 @@
+// components
+export { default as FaqWidget } from './components/FaqWidget/FaqWidget'
+export { default as FaqWidgetExample } from './components/FaqWidget/FaqWidgetExample'
+
+// hooks
+export { default as useFaq } from './hooks/useFaq'
+
+// store
+export { faqStore } from './store/faqStore'
